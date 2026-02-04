cdec9a14726e672243cb3db69d0aa7822dd8b7c8
poyaohuang <poyaohg0705@gmail.com>
2026-02-04 20:06:18 +1100

    chore: Update test modification comment from 5 to 6.

--- app/db/database.py
+++ app/db/database.py
@@ -13,7 +13,7 @@ engine = create_async_engine(
 )
 
 
-# Test modification 5
+# Test modification 6
 async def init_db():
     async with engine.begin() as conn:
         await conn.run_sync(SQLModel.metadata.create_all)
