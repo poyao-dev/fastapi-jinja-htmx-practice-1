74f5ad34cc0be3bc43f66e18af124b0e6f1c0c65
poyaohuang <poyaohg0705@gmail.com>
2026-02-05 18:35:19 +1100

    chore: update test modification comment in database initialization.

--- app/db/database.py
+++ app/db/database.py
@@ -13,7 +13,7 @@ engine = create_async_engine(
 )
 
 
-# Test modification 2
+# Test modification 1
 async def init_db():
     async with engine.begin() as conn:
         await conn.run_sync(SQLModel.metadata.create_all)
