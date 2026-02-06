f4ea7e724cb3a0af3a27eac5abad50ca3a39393c
poyaohuang <poyaohg0705@gmail.com>
2026-02-07 01:38:15 +1100

    test

--- app/db/database.py
+++ app/db/database.py
@@ -13,7 +13,7 @@ engine = create_async_engine(
 )
 
 
-# Test modification test 5
+# Test modification test 6
 async def init_db():
     async with engine.begin() as conn:
         await conn.run_sync(SQLModel.metadata.create_all)
